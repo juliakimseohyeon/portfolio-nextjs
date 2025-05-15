26edfbdc73e69b99c542fa609382644a9b368978
Julia Kim <juliakimseohyeon@gmail.com>
2025-05-15 15:09:05 -0400

    Fix homescreen spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,8 +18,8 @@ export const metadata: Metadata = {
 
 export default function Home() {
 	return (
-		<main className="px-4 pb-8 max-w-full flex flex-col gap-20 md:gap-32">
-			<section className="w-full flex flex-col gap-8 items-center">
+		<main className="pb-16 max-w-full flex flex-col items-center gap-40 md:gap-60">
+			<section className="px-4 w-full flex flex-col gap-8 items-center">
 				<div className="m-0 text-center justify-items-center">
 					<h1 className="bg-gradient-to-r from-blue-300 to-purple-500 bg-clip-text text-transparent font-extrabold">
 						Hi, I'm Julia
@@ -27,7 +27,7 @@ export default function Home() {
 					<h2>Scroll down for the good stuff</h2>
 					<ArrowDown className="animate-bounce w-6 mt-2" />
 				</div>
-				<div className="w-full lg:w-4/5 relative p-4 flex flex-col items-center justify-center text-lg h-80 rounded-lg md:box-border md:p-8 bg-gradient-to-br from-purple-700 to-blue-300">
+				<div className="w-full lg:w-3/5 relative p-4 flex flex-col items-center justify-center text-lg h-80 rounded-lg md:box-border md:p-8 bg-gradient-to-br from-purple-700 to-blue-300">
 					<div className="flex flex-row justify-start gap-2 max-w-full absolute top-4 left-4 md:top-8 md:left-8">
 						<div className="size-4 rounded-full bg-red-300" />
 						<div className="size-4 rounded-full bg-yellow-300" />
@@ -48,46 +48,37 @@ export default function Home() {
 					</div>
 				</div>
 			</section>
-			<section className="max-w-full md:w-4/5 lg:w-full flex flex-col items-center gap-4">
+			<section className="w-full flex flex-col items-center gap-4">
 				<h2>Skills</h2>
 				<LogoCarousel />
 			</section>
-			<section className="max-w-full md:w-4/5 lg:w-full flex flex-col items-center gap-4">
+			<section className="px-4 max-w-full lg:w-4/5 flex flex-col items-center gap-8">
 				<h2>Projects</h2>
-				<div className="w-full flex flex-col justify-between items-center gap-4 text-left relative">
+				<div className="w-full flex flex-col lg:flex-row flex-nowrap justify-center items-center gap-4 text-left relative">
 					{projects.map((project) => (
 						<Link
 							href={`/projects/${project.id}`}
-							className="w-full lg:w-3/5 flex flex-col lg:flex-row justify-between items-start lg:items-stretch lg:flex-1 text-left relative bg-blue-300/20 rounded-lg p-4 box-border gap-6 transition duration-300 hover:bg-blue-300/40 hover:scale-101"
+							className="group w-full lg:w-[calc(50%-0.5rem)] flex flex-col items-center justify-center relative rounded-lg hover:scale-101 transition duration-300"
 							key={project.id}
 						>
 							<Image
-								className="w-full rounded-lg object-cover lg:flex-none lg:w-1/2"
+								className="w-full rounded-lg object-cover opacity-50 lg:opacity-100 lg:group-hover:opacity-30 transition duration-300"
 								src={project.image}
 								alt={project.project_name}
 							/>
-							<div className="lg:flex-1 lg:w-full lg:flex lg:flex-col lg:justify-between lg:items-start">
-								<h3>{project.project_name}</h3>
-								<p className="mb-6">{project.intro_text}</p>
-								<div className="flex flex-row justify-start items-start flex-wrap gap-2 pb-6">
-									{project.tags.map((tag) => (
-										<p
-											className="m-0 leading-1.5 text-xs text-white bg-blue-300 bg-gradient-to-br from-blue-300/50 to-purple-700/50 p-2 rounded-3xl"
-											key={project.id}
-										>
-											{tag}
-										</p>
-									))}
-								</div>
-								<Button className="w-full bg-blue-300 text-black">
-									Read about {project.project_name}
-								</Button>
-							</div>
+							<h3 className="text-center absolute lg:hidden lg:group-hover:block">
+								{project.project_name}
+							</h3>
 						</Link>
 					))}
 				</div>
+				<Link href="/projects" className="w-full lg:w-fit">
+					<Button className="w-full bg-white text-black hover:bg-blue-300 hover:text-white cursor-pointer">
+						View All Projects
+					</Button>
+				</Link>
 			</section>
-			<section className="max-w-full md:w-4/5 lg:w-full flex flex-col items-center gap-4">
+			<section className="px-4 max-w-full md:w-4/5 flex flex-col items-center gap-8">
 				<h2>Words on the Street</h2>
 				<div className="w-full flex flex-col lg:flex-row justify-between items-center gap-4 lg:flex-wrap text-left relative">
 					{testimonials.map((testimonial) => (
