ba48f9fde54c7824b5f19449d01cc68e53173ac1
Julia Kim <juliakimseohyeon@gmail.com>
2025-05-15 16:28:07 -0400

    Add HeroParallax component from Aceternity

--- src/app/graphic-design/data/graphicDesignData.tsx
+++ src/app/graphic-design/data/graphicDesignData.tsx
@@ -22,37 +22,39 @@ import imgRoyals4 from "../../../../public/images/graphic-projects/graphics-roya
 
 export const graphicProjects = [
 	{
-		id: 0,
-		project_name: "GetCrisp",
-		images: [imgGetCrispArcus, imgGetCrispK5, imgGetCrispRhm],
-		tags: ["Social Media Ads"],
+		title: "GetCrisp Digital Marketing Agency",
+		link: "https://www.getcrisp.co.uk/",
+		thumbnail: "/images/graphic-projects/graphics-getcrisp-arcus.jpg",
 	},
 
 	{
-		id: 1,
-		project_name: "Nest by Chard Development",
-		images: [
-			imgNest1,
-			imgNest2,
-			imgNest3,
-			imgNest4,
-			imgNest5,
-			imgNest6,
-			imgNest7,
-			imgNest8,
-		],
-		tags: ["Environmental Graphics", "Illustrations", "Branding", "Print Ads"],
+		title: "Nest by Chard Development",
+		link: "https://www.nestbychard.com/",
+		thumbnail: "/images/graphic-projects/graphics-nest-1.jpg",
 	},
 	{
-		id: 2,
-		project_name: "The Royals by Chard Development",
-		images: [imgRoyals1, imgRoyals2, imgRoyals3, imgRoyals4],
-		tags: ["Branding", "Print Ads"],
+		title: "The Royals by Chard Development",
+		link: "https://www.theroyalsbychard.com/",
+		thumbnail: "/images/graphic-projects/graphics-royals-1.jpg",
 	},
 	{
-		id: 3,
-		project_name: "Vancouver Harbour Centre",
-		images: [imgHc1, imgHc2, imgHc3, imgHc4, imgHc5, imgHc6],
-		tags: ["Environmental Graphics", "Illustrations", "Social Media Ads"],
+		title: "Vancouver Harbour Centre",
+		link: "https://www.vancouverharbourcentre.com/",
+		thumbnail: "/images/graphic-projects/graphics-hc-6.jpg",
+	},
+	{
+		title: "Aire at North Harbour by Concert Properties",
+		link: "https://www.vancouverharbourcentre.com/",
+		thumbnail: "/images/graphic-projects/graphics-aire-1.jpg",
+	},
+	{
+		title: "Kevington U",
+		link: "https://www.vancouverharbourcentre.com/",
+		thumbnail: "/images/graphic-projects/graphics-kevington-u-4.jpg",
+	},
+	{
+		title: "Stanley Park Brewing",
+		link: "https://www.vancouverharbourcentre.com/",
+		thumbnail: "/images/graphic-projects/graphics-stanley-park-brewing.jpeg",
 	},
 ];
