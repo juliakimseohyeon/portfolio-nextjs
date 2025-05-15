0f936036a9e3952a965ace46d3f346a4ae376044
Julia Kim <juliakimseohyeon@gmail.com>
2025-05-15 10:51:49 -0400

    Add logos

--- src/app/projects/components/CarouselImages.tsx
+++ src/app/projects/components/CarouselImages.tsx
@@ -5,11 +5,18 @@ import logoFigma from "../../../../public/logos/logo-figma.svg";
 import logoHtml from "../../../../public/logos/logo-html.svg";
 import logoJavascript from "../../../../public/logos/logo-javascript.svg";
 import logoMysql from "../../../../public/logos/logo-mysql.svg";
+import logoNest from "../../../../public/logos/logo-nestjs.svg";
+import logoNext from "../../../../public/logos/logo-next.svg";
 import logoNode from "../../../../public/logos/logo-node.svg";
 import logoOpenai from "../../../../public/logos/logo-openai-icon.svg";
 import logoPostman from "../../../../public/logos/logo-postman.svg";
+import logoPrisma from "../../../../public/logos/logo-prisma.svg";
 import logoReact from "../../../../public/logos/logo-react.svg";
 import logoSass from "../../../../public/logos/logo-sass.svg";
+import logoShadcn from "../../../../public/logos/logo-shadcn.svg";
+import logoTailwindCss from "../../../../public/logos/logo-tailwindcss.svg";
+import logoTypescript from "../../../../public/logos/logo-typescript.svg";
+import logoVercel from "../../../../public/logos/logo-vercel.svg";
 import logoXd from "../../../../public/logos/logo-xd.svg";
 
 export const carouselImages = [
@@ -26,4 +33,11 @@ export const carouselImages = [
 	logoExpress,
 	logoOpenai,
 	logoPostman,
+	logoVercel,
+	logoNext,
+	logoNest,
+	logoShadcn,
+	logoTypescript,
+	logoPrisma,
+	logoTailwindCss,
 ];
