06202b721e0c15a4b5193e982e5e6976559595d4
Julia Kim <juliakimseohyeon@gmail.com>
2025-05-15 16:31:41 -0400

    Remove unused lines of code

--- src/app/graphic-design/data/graphicDesignData.tsx
+++ src/app/graphic-design/data/graphicDesignData.tsx
@@ -1,60 +1,43 @@
-import imgGetCrispArcus from "../../../../public/images/graphic-projects/graphics-getcrisp-arcus.jpg";
-import imgGetCrispK5 from "../../../../public/images/graphic-projects/graphics-getcrisp-k5.jpg";
-import imgGetCrispRhm from "../../../../public/images/graphic-projects/graphics-getcrisp-rhm.jpg";
-import imgHc1 from "../../../../public/images/graphic-projects/graphics-hc-1.jpg";
-import imgHc2 from "../../../../public/images/graphic-projects/graphics-hc-2.jpg";
-import imgHc3 from "../../../../public/images/graphic-projects/graphics-hc-3.jpg";
-import imgHc4 from "../../../../public/images/graphic-projects/graphics-hc-4.jpg";
-import imgHc5 from "../../../../public/images/graphic-projects/graphics-hc-5.jpg";
-import imgHc6 from "../../../../public/images/graphic-projects/graphics-hc-6.jpg";
-import imgNest1 from "../../../../public/images/graphic-projects/graphics-nest-1.jpg";
-import imgNest2 from "../../../../public/images/graphic-projects/graphics-nest-2.jpg";
-import imgNest3 from "../../../../public/images/graphic-projects/graphics-nest-3.jpg";
-import imgNest4 from "../../../../public/images/graphic-projects/graphics-nest-4.jpg";
-import imgNest5 from "../../../../public/images/graphic-projects/graphics-nest-5.jpg";
-import imgNest6 from "../../../../public/images/graphic-projects/graphics-nest-6.jpg";
-import imgNest7 from "../../../../public/images/graphic-projects/graphics-nest-7.jpg";
-import imgNest8 from "../../../../public/images/graphic-projects/graphics-nest-8.jpg";
-import imgRoyals1 from "../../../../public/images/graphic-projects/graphics-royals-1.jpg";
-import imgRoyals2 from "../../../../public/images/graphic-projects/graphics-royals-2.jpg";
-import imgRoyals3 from "../../../../public/images/graphic-projects/graphics-royals-3.jpg";
-import imgRoyals4 from "../../../../public/images/graphic-projects/graphics-royals-4.jpg";
-
 export const graphicProjects = [
 	{
-		title: "GetCrisp Digital Marketing Agency",
-		link: "https://www.getcrisp.co.uk/",
+		title: "Arcus Communications",
 		thumbnail: "/images/graphic-projects/graphics-getcrisp-arcus.jpg",
 	},
 
 	{
 		title: "Nest by Chard Development",
-		link: "https://www.nestbychard.com/",
 		thumbnail: "/images/graphic-projects/graphics-nest-1.jpg",
 	},
 	{
 		title: "The Royals by Chard Development",
-		link: "https://www.theroyalsbychard.com/",
 		thumbnail: "/images/graphic-projects/graphics-royals-1.jpg",
 	},
 	{
 		title: "Vancouver Harbour Centre",
-		link: "https://www.vancouverharbourcentre.com/",
 		thumbnail: "/images/graphic-projects/graphics-hc-6.jpg",
 	},
 	{
 		title: "Aire at North Harbour by Concert Properties",
-		link: "https://www.vancouverharbourcentre.com/",
 		thumbnail: "/images/graphic-projects/graphics-aire-1.jpg",
 	},
 	{
+		title: "RHM Communications",
+		thumbnail: "/images/graphic-projects/graphics-getcrisp-rhm.jpg",
+	},
+	{
 		title: "Kevington U",
-		link: "https://www.vancouverharbourcentre.com/",
 		thumbnail: "/images/graphic-projects/graphics-kevington-u-4.jpg",
 	},
 	{
 		title: "Stanley Park Brewing",
-		link: "https://www.vancouverharbourcentre.com/",
 		thumbnail: "/images/graphic-projects/graphics-stanley-park-brewing.jpeg",
+	},
+	{
+		title: "K5 Communications",
+		thumbnail: "/images/graphic-projects/graphics-getcrisp-k5.jpg",
+	},
+	{
+		title: "Tapestry by Concert Properties",
+		thumbnail: "/images/graphic-projects/graphics-tapestry.jpeg",
 	},
 ];
