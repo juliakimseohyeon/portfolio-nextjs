90f7acad40cb90092026aa5c9c96259498c65f2c
Julia Kim <juliakimseohyeon@gmail.com>
2025-06-13 03:23:33 -0400

    Use real emoji instead of UTF code

--- src/app/projects/components/TypewriterComponent.tsx
+++ src/app/projects/components/TypewriterComponent.tsx
@@ -4,23 +4,25 @@ import Typewriter from "typewriter-effect";
 
 export default function TypewriterComponent() {
 	return (
-		<Typewriter
-			onInit={(typewriter) => {
-				typewriter
-					.typeString('likes: ["\u{1F63B}", "\u{1F33F}"]')
-					.pauseFor(1000)
-					.deleteAll()
-					.typeString('hobbies: ["\u{1F97E}", "\u{1F3D5}\u{FE0F}"]')
-					.pauseFor(1000)
-					.deleteAll()
-					.start();
-			}}
-			options={{
-				loop: true,
-				delay: 75,
-				deleteSpeed: 50,
-				wrapperClassName: "font-extralight font-emoji",
-			}}
-		/>
+		<div className="font-emoji">
+			<Typewriter
+				onInit={(typewriter) => {
+					typewriter
+						.typeString('likes: ["😻", "🌿"]')
+						.pauseFor(1000)
+						.deleteAll()
+						.typeString('hobbies: ["🥾", "🏕️"]')
+						.pauseFor(1000)
+						.deleteAll()
+						.start();
+				}}
+				options={{
+					loop: true,
+					delay: 75,
+					deleteSpeed: 50,
+					wrapperClassName: "font-emoji font-thin",
+				}}
+			/>
+		</div>
 	);
 }
