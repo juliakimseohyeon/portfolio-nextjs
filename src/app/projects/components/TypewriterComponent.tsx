9a0d252378acf0ac5925cf6a35af5943c90aa9ce
Julia Kim <juliakimseohyeon@gmail.com>
2025-06-13 03:33:58 -0400

    Add a more comprehensive emoji font fallbacks to ensure better cross-platform compatibility

--- src/app/projects/components/TypewriterComponent.tsx
+++ src/app/projects/components/TypewriterComponent.tsx
@@ -5,24 +5,26 @@ import Typewriter from "typewriter-effect";
 export default function TypewriterComponent() {
 	return (
 		<div className="font-emoji">
-			<Typewriter
-				onInit={(typewriter) => {
-					typewriter
-						.typeString('likes: ["😻", "🌿"]')
-						.pauseFor(1000)
-						.deleteAll()
-						.typeString('hobbies: ["🥾", "🏕️"]')
-						.pauseFor(1000)
-						.deleteAll()
-						.start();
-				}}
-				options={{
-					loop: true,
-					delay: 75,
-					deleteSpeed: 50,
-					wrapperClassName: "font-emoji font-thin",
-				}}
-			/>
+			<div className="inline-block">
+				<Typewriter
+					onInit={(typewriter) => {
+						typewriter
+							.typeString('likes: ["😻", "🌿"]')
+							.pauseFor(1000)
+							.deleteAll()
+							.typeString('hobbies: ["🥾", "🏕️"]')
+							.pauseFor(1000)
+							.deleteAll()
+							.start();
+					}}
+					options={{
+						loop: true,
+						delay: 75,
+						deleteSpeed: 50,
+						wrapperClassName: "font-emoji font-thin inline-block",
+					}}
+				/>
+			</div>
 		</div>
 	);
 }
